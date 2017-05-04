cbb5392f0039b197100c3233c702278a71424b1a
Jason Loucel <jloucel@gmail.com>
2017-05-04 17:11:10 -0600

    Add files via upload

--- sierpinski.elm
+++ sierpinski.elm
@@ -1,84 +1,105 @@
-import Html exposing (Html, beginnerProgram, div, button, text, br, input)
-import Html.Attributes exposing (type_, min, max, value)
+{-
+  An example of how to utilize Elm to generate Sirpenski's triange.
+  At order 0 there is are no triangles, at order 10 there would be
+  59,049 triangles. This implimentation is limited to 10 iterations
+  due to performnace limitations.
+-}
+
+import Html exposing (Html, beginnerProgram, div, button, text, br, input, em)
+import Html.Attributes exposing (type_, min, max, value, style)
 import Html.Events exposing (onInput)
-import Svg exposing (svg, polygon)
+import Svg exposing (Svg, svg, polygon)
 import Svg.Attributes exposing (fill, stroke, points, width, height, viewBox)
 
-width = 400
-height = 400
-plist = "200,10 10,390 390,390"
+vWidth = 400
+vHeight = 400
+
+type alias Triangle = (Point, Point, Point)
 
 type alias Point =
   { x : Float, y : Float}
 
+{-
+   The following three functions are provided
+   to allow ViewBox to be dynamically sized.
+   they return formatted strings for
+   vWidth and vHeight.
+-}
+
+-- get viewBox dimentions string
+getViewDims : Int -> Int -> String
+getViewDims w h =
+  "0 0 " ++ toString w ++ " " ++ toString h
+
+-- get vWidth string
+getvWidth : Int -> String
+getvWidth w =
+  toString w ++ "px"
+
+-- get vHeight String
+getvHeight : Int -> String
+getvHeight h =
+  toString h ++ "px"
+
 -- return new point  midpoint
 getMidPoint: Point -> Point -> Point
 getMidPoint p1 p2 =
-    {x = ((p1.x + p2.x) / 2.0), y = ((p1.y + p2.y) / 2.0)}
+  {x = ((p1.x + p2.x) / 2.0), y = ((p1.y + p2.y) / 2.0)}
 
 -- return a new polygon with dim p
-genNextTriangle p =
-  polygon [fill "none", stroke "black", points p][]
+genTriangle : Triangle -> Svg Msg
+genTriangle triangle =
+  let
+    (p1, p2, p3) = triangle
+    pointString = String.join " " [pointToString p1, pointToString p2, pointToString p3]
+  in
+    polygon [fill "none", stroke "black", points pointString][]
 
+-- generates the initial points of the fractal
+genInitialPoints: Int -> Int -> Int -> (Point, Point, Point)
+genInitialPoints order w h =
+  let
+    p1 = {x = (toFloat w / 2.0), y = (toFloat 10)}
+    p2 = {x = (toFloat 10), y =  toFloat (h - 10)}
+    p3 = {x = toFloat (w - 10), y =  toFloat (h - 10)}
+ in
+    (p1,p2,p3)
 
-{-
-  So I'm trying to figure out how I can make a
-  functional implimentation for what I need to do.
+-- takes a tuple of points and returns a list of sub points
+genSubTriangles: Triangle -> (Triangle, Triangle,Triangle)
+genSubTriangles plist =
+  let (p1,p2,p3) = plist
+      p12 = getMidPoint p1 p2
+      p23 = getMidPoint p2 p3
+      p31 = getMidPoint p3 p1
+  in
+      ((p1, p12, p31),(p12, p2, p23),(p31, p23, p3))
 
-  genPoints will generate the initial triangle, order 0
-  of the set. It returns a string currently, what I would like
-  to do is push that output to genNextTriangle and have it return
-  the poloygon object.
+-- takes a tuple of tuples and returns a list of polygons, recursive
+genManyTriangles : Int -> Triangle -> List (Svg Msg)
+genManyTriangles order triangle =
+  case order of
+    0 -> []
+    _ -> let (t1, t2, t3) = genSubTriangles triangle
+         in genTriangle triangle :: (List.concat [genManyTriangles (order - 1) t1,
+             genManyTriangles (order - 1) t2,
+             genManyTriangles (order - 1) t3
+            ])
 
-  The else case for the genPoints would process recursively and return
-  a list of polygon objects following the logic commented in the else case.
-  The problem is that I need to change the gen points to accept points if I
-  were to generate the initial points in another fucntion so that I can use
-  this recursively to generate the points for each order and add to a list.
-  The list can be returned and update the view, svg accepts a list of shapes
-  so it will work well for what I want to do.
+-- convert a point to string
+pointToString : Point -> String
+pointToString p =
+  toString p.x ++ "," ++ toString p.y
 
-  So question is should I store the initial points in model? They are only
-  used once.
--}
-
-
-
-
---generate points string
-genPoints: Int -> Int -> Int -> String
-genPoints order w h =
-    let
-        pointToString : Point -> String
-        pointToString p =
-            toString p.x ++ "," ++ toString p.y
-    in
-        if order == 0 then
-            let
-                p1 = {x = (toFloat w / 2.0), y = (toFloat 10)}
-                p2 = {x = (toFloat 10), y =  toFloat (h - 10)}
-                p3 = {x = toFloat (w - 10), y =  toFloat (h - 10)}
-            in
-                [ p1, p2, p3 ]
-                    |> List.map pointToString
-                    |> String.join " "
-        else
-          ""
-{-
-                p12 = midpoint p1 p2
-                p23 = midpoint p2 p3
-                p31 = midpoint p3 p1
-
-                genPoints (order-1) p1 p12 p31
-                genPoints (order-1) p12 p2 p23
-                genPoints (order-1) p31 p23 p3
-
-
--}
+-- entry point to Sirpenski generation
+genPolys : Int -> Int -> Int -> List (Svg Msg)
+genPolys order w h =
+  genInitialPoints order w h
+  |> genManyTriangles order
 
 main =
   Html.beginnerProgram
-  { model = initialmodel
+  { model = model
   , view = view
   , update = update
   }
@@ -86,30 +107,22 @@ main =
 -- model
 
 type alias Model =
-  { fracOrder : Int
-  , polyPoints : String
- }
+  { fracOrder : Int }
 
-
-initialmodel : Model
-initialmodel =
- Model 0 plist
-
+model : Model
+model =
+ Model 0
 
 -- update
 
 type Msg
-    = Update String
+  = Update String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-
     Update fracOrder ->
-      { model |
-        fracOrder = (Result.withDefault 0 (String.toInt fracOrder))
-      , polyPoints = "100,10 10,290 290,290"
-      }
+      { model | fracOrder = (Result.withDefault 0 (String.toInt fracOrder)) }
 
 -- view
 
@@ -118,17 +131,15 @@ view model =
 
   div []
       [
-      svg [ viewBox "0 0 400 400",  Svg.Attributes.height "400px", Svg.Attributes.width "400px" ]
-      [
-      -- here will be the function that returns a polygon list
-        genNextTriangle model.polyPoints
-      ]
+      div [] [Html.text "Sirpenski's Triangle"]
+      , svg [ viewBox (getViewDims vHeight vWidth),  Svg.Attributes.height (getvHeight vHeight), Svg.Attributes.width (getvWidth vWidth)]
+      (genPolys model.fracOrder vWidth vHeight)
       , br [][]
       , br [][]
       , div [] [ Html.text "Fractal Order"]
       , input [ Html.Attributes.type_ "range", Html.Attributes.min "0"
-                                               , Html.Attributes.max "10"
-                                               , value <| toString model.fracOrder
-                                               , onInput Update] []
+                                             , Html.Attributes.max "10"
+                                             , value <| toString model.fracOrder
+                                             , onInput Update] []
       , Html.text <| toString model.fracOrder
-    ]
+      ]
